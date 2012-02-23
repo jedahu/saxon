98e7779143c70caccb50f66a8c47a776be475501
Jeremy Hughes <jedahu@gmail.com>
2012-02-23 14:27:21 +1300

    use org.clojars.jedahu group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-saxon "0.9.3-SNAPSHOT"
+(defproject org.clojars.jedahu/clojure-saxon "0.9.3-SNAPSHOT"
             :description "Clojure wrapper for the Saxon XSLT and XQuery processor."
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [net.sf.saxon/saxon "9.4.0.2"]])
